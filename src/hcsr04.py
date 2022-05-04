bcdc1087513e9064c36a48fe0b3ee27f77aa6ae5
Igor de Brito Coura <igor-coura@hotmail.com>
2022-05-04 18:22:21 -0300

    create code to HCSR04

---
+++ src/hcsr04.py
@@ -0,0 +1,21 @@
+from machine import Pin
+import utime
+
+class HCSR04:
+    def __init__(self, trig_pin, echo_pin):
+        self.trigger = Pin(trig_pin, Pin.OUT)
+        self.echo = Pin(echo_pin, Pin.IN)
+    def distance_cm(self):
+        self.trigger.low()
+        utime.sleep_us(2)
+        self.trigger.high()
+        utime.sleep_us(5)
+        self.trigger.low()
+        while self.echo.value() == 0:
+            signaloff = utime.ticks_us()
+        while self.echo.value() == 1:
+            signalon = utime.ticks_us()
+        timepassed = signalon - signaloff
+        return (timepassed * 0.0343) / 2
+        
+        
